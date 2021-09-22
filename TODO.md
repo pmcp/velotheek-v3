e78666db5372dc6cca66baf20fdcf46c57811f6a
PMCP <maartenlauwaert@gmail.com>
2021-09-22 11:57:31 +0200

    added logo

--- TODO.md
+++ TODO.md
@@ -1,17 +1,16 @@
 # DOING
+- Add Logo
 
 # TODO
 - Change lang for vue formulate
-- Add Logo
 - Logo's toevoegen voor homepage
 - Change site lang on change lang
 - Admin
     - emails toevoegen
         - Confirmation
-    - rappel email reservation deux jour avant
     - uren kunnen aanpassen in de horaire
 - Toevoegen van een admin calender om dagen vast te leggen wanneer niet beschikbaar
 - drive arrangeren
 
-# ENHANCE!
+# ENHANCE
 - https://katiekodes.com/auth0-netlify-backend/
--- pages/_page.vue
+++ pages/_page.vue
@@ -1,41 +1,40 @@
 <template>
   <main class="relative bg-gray-50">
     <template v-if="content.title === 'Home'">
-      <div class="lg:relative">
-        <div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
-          <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
-            <heading-one>
-              {{ content.intro }}
-            </heading-one>
-            <div class="prose prose-indigo prose-lg text-gray-500 mx-auto">
-              <nuxt-content class="prose lg:prose-xl" :document="content" />
-            </div>
+      <div class="flex flex-col md:flex-row">
+        <div class="relative flex md:w-1/2 mr-4 bg-pink-500 justify-items-stretch items-stretch">
 
-            <!--          <p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">-->
-            <!--            {{ content.body }}-->
-            <!--          </p>-->
-            <div class="mt-10 sm:flex sm:justify-center lg:justify-start" v-if="user">
-              <div class="rounded-md shadow">
-                <nuxt-link
-                  :to="`/locations/4saisons.${lang}`"
-                  class="
-                    bg-pink-100
-                    border-2
-                    text-pink-500
-                    border-pink-500
-                    hover:bg-pink-500 hover:text-pink-100
-                    rounded
-                    p-4
-                  "
-                >
-                  <translation :id="14" class="" />
-                </nuxt-link>
-              </div>
-            </div>
+          <img class="object-cover" :src="content.image" alt="" />
+          <div class="absolute w-full h-full top-0 left-0 flex justify-center items-center p-5">
+            <svg-logo class="text-white" />
           </div>
         </div>
-        <div class="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
-          <img class="absolute inset-0 w-full h-full object-cover" :src="content.image" alt="" />
+        <div class="pb-20 md:w-1/2 ml-4 mt-5">
+          <heading-one>
+            {{ content.intro }}
+          </heading-one>
+          <div class="prose prose-indigo prose-lg text-gray-500 mx-auto">
+            <nuxt-content class="prose lg:prose-xl" :document="content" />
+          </div>
+
+          <div class="mt-10 sm:flex sm:justify-center lg:justify-start" v-if="user">
+            <div class="rounded-md shadow">
+              <nuxt-link
+                :to="`/locations/4saisons.${lang}`"
+                class="
+                  bg-pink-100
+                  border-2
+                  text-pink-500
+                  border-pink-500
+                  hover:bg-pink-500 hover:text-pink-100
+                  rounded
+                  p-4
+                "
+              >
+                <translation :id="14" class="" />
+              </nuxt-link>
+            </div>
+          </div>
         </div>
       </div>
     </template>
